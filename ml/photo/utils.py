028c997637b971fdf38df098093c2d28ea4ab683
Dmitry Barsukoff <riZZZhik@gmail.com>
2022-03-03 10:52:03 +0300

    [ADD] ML Code

--- ml/photo/utils.py
+++ ml/photo/utils.py
@@ -1,46 +1,37 @@
 import os
-
-import scipy.misc
 import numpy as np
-from PIL import Image
+import scipy.misc
+import sys
+
+from cv2 import imread, imwrite
 
 
-def is_image(filename):
-    return any(t.lower() in filename[-4:] for t in ['jpg', 'png', 'jpeg'])
-
+def isimage(str):
+  return any(type in str[-4:] for type in ['jpg', 'png', 'jpeg'])
 
 def save_img(out_path, img):
-    # TODO: If image pixels are floats 0-1 transform to 0-256 uint8
     img = np.clip(img, 0, 255).astype(np.uint8)
-    img = Image.fromarray(img)
-    img.save(out_path)
-
+    imwrite(out_path, img)
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
-    o0, o1 = Image.open(style_path).size
-    o2 = 3  # FIXME
-
+    o0, o1, o2 = imread(style_path, mode='RGB').shape
+    scale = float(style_scale)
     new_shape = (int(o0 * scale), int(o1 * scale), o2)
-    style_target = get_img(style_path, img_size=new_shape)
+    style_target = _get_img(style_path, img_size=new_shape)
     return style_target
 
-
-def get_img(img, img_size=None):
-    if type(img) == str:
-        img = Image.open(img)
-
-    if not (len(img.shape) == 3 and img.shape[2] == 3):
-        img = np.dstack((img, img, img))
-
-    if img_size:
-        img = scipy.misc.imresize(img, img_size)
-    return img
-
+def get_img(img, img_size=False):
+   if type(img) == str:
+     img = imread(img)
+   if not (len(img.shape) == 3 and img.shape[2] == 3):
+       img = np.dstack((img,img,img))
+   if img_size != False:
+       img = scipy.misc.imresize(img, img_size)
+   return img
 
 def exists(p, msg):
     assert os.path.exists(p), msg
-
 
 def list_files(in_path):
     files = []
@@ -50,7 +41,6 @@ def list_files(in_path):
 
     return files
 
-
 def get_files(img_dir):
     files = list_files(img_dir)
-    return [os.path.join(img_dir, x) for x in files]
+    return [os.path.join(img_dir,x) for x in files]
